726b21c8a41a6b1ee9367859d56cf30a003e7c05
guohongtao2 <guohongtao2@guazi.com>
2022-04-13 11:27:28 +0800

    add gateway fallback

---
+++ gateway/src/main/java/org/kaws/gateway/web/controller/FallbackController.java
@@ -0,0 +1,21 @@
+package org.kaws.gateway.web.controller;
+
+import org.kaws.common.reponse.R;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Bosco
+ * @date 2022/4/13 11:12 上午
+ */
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/gateway/fallback")
+    public R fallback() {
+        return R.failure(HttpStatus.TOO_MANY_REQUESTS.value(), HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase());
+    }
+
+}
