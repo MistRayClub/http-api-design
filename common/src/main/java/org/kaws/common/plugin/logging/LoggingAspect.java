a830a09e611dd926cf01aa9eba6d6d3078e20233
guohongtao2 <guohongtao2@guazi.com>
2022-04-14 16:44:10 +0800

    update LoggingAspect

--- common/src/main/java/org/kaws/common/plugin/logging/LoggingAspect.java
+++ common/src/main/java/org/kaws/common/plugin/logging/LoggingAspect.java
@@ -2,6 +2,9 @@ package org.kaws.common.plugin.logging;
 
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.extra.spring.SpringUtil;
+import cn.hutool.extra.ssh.JschUtil;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,18 +13,26 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.kaws.common.annotation.Logging;
 import org.kaws.common.domain.dto.SysLogDTO;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
  * @author Bosco
  * @date 2022/4/8 12:23 上午
  */
 
-@Component
+@Slf4j
 @Aspect
+@Component
 public class LoggingAspect {
 
 
@@ -32,6 +43,24 @@ public class LoggingAspect {
 
     @Around("loggingPointCut()")
     private Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        Function<ServletRequest, String> getRequestBody = request -> {
+            try {
+                int len = request.getContentLength();
+                ServletInputStream inputStream = request.getInputStream();
+                byte[] buffer = new byte[len];
+                inputStream.read(buffer, 0, len);
+                return new String(buffer);
+            } catch (IOException e) {
+                log.error("parse request body error: {}", e.getMessage());
+            }
+            return null;
+        };
+
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert requestAttributes != null;
+        HttpServletRequest request = requestAttributes.getRequest();
+
         SysLogDTO sysLogDTO = new SysLogDTO();
         Object result;
         try {
@@ -41,12 +70,17 @@ public class LoggingAspect {
             sysLogDTO.setTitle(loggingAnnotation.title());
             sysLogDTO.setDescription(loggingAnnotation.describe());
             sysLogDTO.setSuccess(true);
+            sysLogDTO.setOperateUrl(request.getRequestURI());
+            sysLogDTO.setMethod(request.getMethod());
+            sysLogDTO.setRequestParam(request.getQueryString());
+            sysLogDTO.setRequestBody(getRequestBody.apply(request));
             result = joinPoint.proceed();
         } catch (Exception exception) {
             sysLogDTO.setSuccess(false);
             sysLogDTO.setErrorMsg(exception.getMessage());
             throw exception;
         } finally {
+            sysLogDTO.setResponseBody(JSONUtil.toJsonStr(request));
             BlockingQueue<SysLogDTO> tempLogQueue = SpringUtil.getBean("logQueue");
             tempLogQueue.offer(sysLogDTO, 2, TimeUnit.SECONDS);
         }
